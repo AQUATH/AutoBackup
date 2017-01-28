9bc6ed9fd3ef24743527e54a7a5a13248040ef92
Nikos M <soadnick@outlook.com>
2017-01-29 00:35:46 +0200

    Update AutoBackup.py

--- AutoBackup.py
+++ AutoBackup.py
@@ -1,33 +1,34 @@
 import os, shutil
-def music(sloc, dloc, op, sel):
+def music(sloc, dloc, op, twb):
     extm = ['.mp3', '.flac', '.aac', '.wav', '.ape', '.alac', '.m4a', '.ogg', '.aiff', '.aif']
-    exta = ['.jpg', '.png', '.bmp', '.gif', '.jpeg', '.JPG', '.JPEG']
+    exta = ['.jpg', '.png', '.bmp', '.gif', '.jpeg']
     for (path, dirs, files) in os.walk(sloc):
         for file in files:
             name, ext = os.path.splitext(file)
+            ext = str.lower(ext)
             if op == '1' and ext not in extm:
                 continue
-            elif op == '2' and (ext not in exta or name[:13] == 'AlbumArtSmall' or name == 'Thumbnail' or name == 'Folder'):
+            elif op == '2' and (ext not in exta or name[:8] == 'AlbumArt' or name == 'Thumbnail' or name == 'Folder'):
                 continue
-            elif op == '3' and (ext not in (extm and exta) or (ext in exta and (name[:13] == 'AlbumArtSmall' or name == 'Thumbnail' or name == 'Folder'))):
+            elif op == '3' and ((ext not in extm and ext not in exta) or (ext in exta and (name[:8] == 'AlbumArt' or name == 'Thumbnail' or name == 'Folder'))):
                 continue
             name += ext
-            path_ = dloc + '/' + path[(len(sloc)+1):] 
-            check = path_ + '/' + file
+            pch = dloc + '/' + path[(len(sloc)+1):] 
+            fch = pch + '/' + file
             file = path + '/' + file
-            if not os.path.exists(path_):
-                os.makedirs(path_)
-            if not os.path.isfile(check):
+            if not os.path.exists(pch):
+                os.makedirs(pch)
+            if not os.path.isfile(fch):
                 print(name + ' not found, copying..') 
-                shutil.copy(file, path_)
+                shutil.copy(file, pch)
                 print('Copy finished.')
-    if sel == 'y' or 'Y':
-        sel = 'n'
-        music(dloc, sloc, op, sel)
+    if twb == ('y' or 'Y'):
+        twb = 'n'
+        music(dloc, sloc, op, twb)
 while True:
     op = input('Back up: Music - 1, Artwork - 2, Both - 3: ')
-    sel =  input('Two-way backup? y/n: ')
+    twb =  input('Two-way backup? y/n: ')
     sloc = input('Enter Source Location: ')
     dloc = input('Enter Destination Location: ')
-    music(sloc, dloc, op, sel)
+    music(sloc, dloc, op, twb)
     print('Operation finished')
