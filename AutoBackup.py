f2ff2a8cfee3b7359c1451fcf859c9e6544da160
Nikos M <soadnick@outlook.com>
2019-03-13 03:43:17 +0200

    Add files via upload
    RC5

--- AutoBackup.py
+++ AutoBackup.py
@@ -1,95 +1,110 @@
 import os
 import shutil
 
-extMusic = ['.mp3', '.flac', '.aac', '.wav', '.wma', '.ape', '.alac', '.m4a', '.m4b', '.m4p', '.ogg', '.aiff', '.aif']
-extArtwork = ['.jpg', '.png', '.bmp', '.gif', '.jpeg']
-extExtras = ['.m3u', '.m3u8', '.wpl', '.pls', '.asx', '.smi', '.sami', '.xspf', '.txt', '.cue', '.log']
-extBoth = extMusic + extArtwork
+ext_music = ['.mp3', '.flac', '.aac', '.wav', '.wma', '.ape', '.alac', '.m4a', '.m4b', '.m4p', '.ogg', '.aiff', '.aif']
+ext_artwork = ['.jpg', '.png', '.bmp', '.gif', '.jpeg']
+ext_extras = ['.m3u', '.m3u8', '.wpl', '.pls', '.asx', '.smi', '.sami', '.xspf', '.txt', '.cue', '.log']
+ext_both = ext_music + ext_artwork
 
 
-def backup(sourceLocation, destinationLocation, path, file, name, ext):
-    pathCheck = destinationLocation + '\\' + path[(len(sourceLocation)+1):]
-    fileCheck = pathCheck + '\\' + file
-    file = path + '\\' + file
-    if not os.path.exists(pathCheck):
+def backup(destination_location, path, file):
+    target_path = destination_location + os.path.sep + path[(len(destination_location)+1):]
+    target_file = os.path.join(target_path, file)
+
+    print(destination_location)
+    print(path[(len(destination_location)+1):])
+    print(target_path)
+
+    if not os.path.exists(target_path):
         try:
-            os.makedirs(pathCheck)
+            os.makedirs(target_path)
         except OSError:
-            print('Unable to create the appropriate folder for', name + ext)
+            print('Unable to create the appropriate folder for', file)
             return 3
-    if not os.path.isfile(fileCheck):
-        print(name + ext, 'not found, copying..')
+
+    if not os.path.isfile(target_file):
+        print(file, 'not found, copying..')
         try:
-            shutil.copy2(file, pathCheck)
+            shutil.copy2(os.path.join(path, file), target_path)
             print('Copy finished.')
             return 1  # Returned when the file was not found, and the copy was successful.
         except shutil.Error:
             print('Copy failed.')
             return 2  # Returned when the file was not found, but the copy failed.
-    return 0  # Returned when the file already exists in destinationLocation.
+
+    return 0  # Returned when the file already exists in destination_location.
 
 
-def createLog(destinationLocation, sourceLocation):
+def create_log(destination_location, source_location):
     from datetime import datetime
-    if not os.path.exists(destinationLocation):
+
+    if not os.path.exists(destination_location):
         try:
-            os.makedirs(destinationLocation) 
+            os.makedirs(destination_location) 
         except OSError:
-            print('\tUnable to create a log file to:', destinationLocation, 'a log file will be saved to:',
-                  sourceLocation, 'instead.')
-            with open(sourceLocation + '\\AutoBackup.log', 'a') as log:
+            print('\tUnable to create a log file to:', destination_location, 'a log file will be saved to:',
+                  source_location, 'instead.')
+            with open(os.path.join(source_location, 'AutoBackup.log'), 'a') as log:
                 log.write('\tDate and time: ' + str(datetime.now())[:19] + '\n\n')
             return 1
-    print('\tA log file will be saved to:', destinationLocation)
-    with open(destinationLocation + '\\AutoBackup.log', 'a') as log:
+
+    print('\tA log file will be saved to:', destination_location)
+
+    with open(os.path.join(destination_location, 'AutoBackup.log'), 'a') as log:
         log.write('\tDate and time: ' + str(datetime.now())[:19] + '\n\n')
+
     return 0
 
 
-def updateLog(location, name, ext, backupResult):
-    with open(location + '\\AutoBackup.log', 'a', encoding='UTF-8') as log:
-        if backupResult == 1:
-            log.write(name + ext + ' was copied successfully.\n')
-        elif backupResult == 2:
-            log.write('Unable to copy ' + name + ext + ' because an error occured.\n')
-        elif backupResult == 3:
-            log.write('Unable to create the appropriate folder for ' + name + ext + '\n')
+def update_log(location, file, backup_result):
+    with open(os.path.join(location, 'AutoBackup.log'), 'a', encoding='UTF-8') as log:
+        if backup_result == 1:
+            log.write(file + ' was copied successfully.\n')
+        elif backup_result == 2:
+            log.write('Unable to copy ' + file + ' because an error occurred.\n')
+        elif backup_result == 3:
+            log.write('Unable to create the appropriate folder for ' + file + '\n')
 
 
-def main(sourceLocation, destinationLocation, operation, extras, twoWayBackup, createLogCheck):
-    if createLogCheck == 'y':
-        createLogResult = createLog(destinationLocation, sourceLocation)
-        logLocation = [destinationLocation, sourceLocation]
-    arrays = [extMusic, extArtwork, extBoth]
-    for path, _, files in os.walk(sourceLocation):
+def main(source_location, destination_location, operation, extras, two_way_backup, enable_log):
+    if enable_log == 'y':
+        create_log_result = create_log(destination_location, source_location)
+        log_location = [destination_location, source_location]
+
+    arrays = [ext_music, ext_artwork, ext_both]
+
+    for path, _, files in os.walk(source_location):
         for file in files:
             name, ext = os.path.splitext(file)
             ext = str.lower(ext)
-            if ext in arrays[int(operation)-1] or ext in extExtras and extras == 'y':
-                if ext not in extArtwork or name[:8] != 'AlbumArt' and name != 'Thumbnail' and name != 'Folder':
-                    backupResult = backup(sourceLocation, destinationLocation, path, file, name, ext)
-                    if backupResult != 0 and createLogCheck == 'y':
-                        updateLog(logLocation[createLogResult], name, ext, backupResult)
-    if twoWayBackup == 'y':
-        main(destinationLocation, sourceLocation, operation, extras, 'n', createLogCheck)
+            print(ext)
+            if ext in arrays[int(operation)-1] or ext in ext_extras and extras == 'y':
+                if ext not in ext_artwork or name[:8] != 'AlbumArt' and name != 'Thumbnail' and name != 'Folder':
+                    backup_result = backup(destination_location, path, file)
+
+                    if backup_result != 0 and enable_log == 'y':
+                        update_log(log_location[create_log_result], file, backup_result)
+
+    if two_way_backup == 'y':
+        main(destination_location, source_location, operation, extras, 'n', enable_log)
 
 
 while True:
     print('Back up: \n\tMusic - 1\n\tArtwork - 2\n\tBoth - 3\nExit - 0')
-    print('Back up extras? (Playlists, logs, texts) y/n')
+    print('Back up extras? (play-lists, logs, texts) y/n')
     print('Two-way backup? y/n')
     print('Save a log file? y/n')
+
     selection = input('Enter your selection in a single line: ')
-    if selection[0] == '1' or selection[0] == '2' or selection[0] == '3':
-        sourceLocation = input('Enter Source Location: ')
-        destinationLocation = input('Enter Destination Location: ')
-        if sourceLocation[-1] == '\\':
-            sourceLocation = sourceLocation[:-1]
-        if destinationLocation[-1] == '\\':
-            destinationLocation = destinationLocation[:-1]
-        main(sourceLocation, destinationLocation, selection[0], str.lower(selection[1]), str.lower(selection[2]),
+
+    if selection[0] in ['1', '2', '3']:
+        source = input('Enter Source Location: ')
+        destination = input('Enter Destination Location: ')
+
+        main(source, destination, selection[0], str.lower(selection[1]), str.lower(selection[2]),
              str.lower(selection[3]))
-        print('\tOperation finished')
+
+        print('\tOperation finished.')
     elif selection[0] == '0':
         break
     else:
